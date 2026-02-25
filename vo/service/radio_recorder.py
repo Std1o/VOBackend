c7ec7ab66c3fc0274fb05f07d115dbddd7637214
Std1o <37378410+Std1o@users.noreply.github.com>
2026-02-25 18:25:40 +0300

    changed time format in radio_recorder

--- vo/service/radio_recorder.py
+++ vo/service/radio_recorder.py
@@ -181,7 +181,7 @@ class RecordingSession:
         self._lock = asyncio.Lock()
 
         # Генерируем имя файла: records/channel_123_20240101_153045_abc123.mp3
-        timestamp = self.start_time.strftime("%Y%m%d_%H%M%S")
+        timestamp = self.start_time.strftime("%d.%m.%Y %H:%M")
         self.filename = f"channel_{channel_id}_{speaker_name}_{timestamp}.mp3"
         self.filepath = os.path.join(records_dir, self.filename)
 
