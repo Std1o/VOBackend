3980f92337763011a82e7f9bd9d0ae71c9eb918a
Std1o <37378410+Std1o@users.noreply.github.com>
2026-02-26 19:10:52 +0300

    вернул

--- vo/api/auth.py
+++ vo/api/auth.py
@@ -22,5 +22,5 @@ def get_user(user: User = Depends(get_current_user)):
     return user
 
 @router.post('/change_name', response_model=User)
-async def change_name(new_name: str, user_id: int, service: AuthService = Depends()):
-    return await service.change_name(user_id, new_name)
+async def change_name(new_name: str, user: User = Depends(get_current_user), service: AuthService = Depends()):
+    return await service.change_name(user.id, new_name)
